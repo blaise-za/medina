1b1788a3865a7fa3a97a0428537ec68199929cea
blaise <blaiseemail@icloud.com>
2024-03-07 21:03:31 +0100

    v0.2

--- sendmails.sh
+++ sendmails.sh
@@ -1,4 +1,37 @@
-#!bin
+#!/bin/bash
 
-boundary=$(uuidgen) && \
-  jinja2 -D recipient=blaise@me.com -D subject="test email" -D boundary=$boundary message.txt.jinja2 > message.txt && uuencode -m consular-service-request-form-completed.pdf consular-service-request-form-completed.pdf >> message.txt && echo "" >> message.txt && echo "--$boundary--" >> message.txt
+while IFS=',' read -r code_mms firstname name class ranking student_nb average recipient file_path filename from cc; do
+  boundary=$(uuidgen)
+  echo $firstname, $name, $class, $ranking, $student_nb, $average, $code_mms, $file_path, $filename, $from, $cc, $recipient, $boundary
+  # jinja2 \
+  #   -D recipient="$recipient" \
+  #   -D subject="test email" \
+  #   -D boundary=$boundary \
+  #   message.txt.jinja2 > message.txt
+done < resources/data.csv
+
+    # -D firstname=$firstname \
+    # -D name=$name \
+    # -D class=$class \
+    # -D ranking=$ranking \
+    # -D student_nb=$student_nb \
+    # -D average=$average \
+    # -D code_mms=$code_mms \
+    # -D file_path=$file_path \
+    # -D filename=$filename \
+    # -D from=$from \
+    # -D cc=$cc \
+
+
+# recipient=blaise@me.com
+# filepath=consular-service-request-form-completed.pdf
+
+# jinja2 \
+#   -D recipient=$recipient \
+#   -D subject="test email" \
+#   -D boundary=$boundary \
+#   message.txt.jinja2 > message.txt
+  
+# uuencode -m $filepath $(basename $filepath) >> message.txt
+# echo "" >> message.txt
+# echo "--$boundary--" >> message.txt
