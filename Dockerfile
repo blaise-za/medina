15b1b1c3357940c02cb808f94aac9a8ef0454422
blaise <blaiseemail@icloud.com>
2024-03-08 17:22:28 +0100

    use of j2

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM dmitryrck/msmtp
 
 RUN apt update && apt upgrade -y
-RUN apt install -y python3-pip sharutils uuid-runtime
-RUN pip3 install jinja2-cli
+RUN apt install -y python3-pip sharutils uuid-runtime recode
+RUN pip3 install jinja2-cli j2cli
 
 ENTRYPOINT [ "/bin/bash" ]
--- sendmails.sh
+++ sendmails.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 boundary=$(uuidgen)
-tail -n+2 resources/data.csv | while IFS=',' read -r codemms firstname name class ranking student_nb average recipient file_path filename from cc; do
-  # echo $boundary
-  echo "$codemms, $firstname, $name, $class, $ranking, $student_nb, $average, $recipient, $file_path, $filename, $from, $cc"
-  echo $recipient
 
-  jinja2 -D recipient=$recipient \
-          -D subject="test email" \
-          -D boundary=$boundary \
-          message.txt.jinja2 > message.txt
+while IFS=',' read -r codemms firstname name class ranking student_nb average recipient file_path filename from cc
+  do
+    export recipient=$recipient
+    export cc=$cc
+    export code_mms=$codemms
+    export file_path=$file_path
+    export filename=$filename
+    export from=$from
+    export firstname=$firstname
+    export name=$name
+    export class=$class
+    export ranking=$ranking
+    export student_nb=$student_nb
+    export average=$average
+    export boundary=$boundary
+    export subject="test email"
+  
+    j2 --format=env message.txt.jinja2 > message.txt
+    # uuencode -m $filepath $(basename $filepath) >> message.txt
+    # echo "" >> message.txt
+    # echo "--$boundary--" >> message.txt
 
-  break
-done
+  done < <(tail -n +2 resources/data.csv)
 
-    # -D firstname=$firstname \
-    # -D name=$name \
-    # -D class=$class \
-    # -D ranking=$ranking \
-    # -D student_nb=$student_nb \
-    # -D average=$average \
-    # -D code_mms=$code_mms \
-    # -D file_path=$file_path \
-    # -D filename=$filename \
-    # -D from=$from \
-    # -D cc=$cc \
-
-
-# recipient=blaise@me.com
-# filepath=consular-service-request-form-completed.pdf
-
-# jinja2 \
-#   -D recipient=$recipient \
-#   -D subject="test email" \
-#   -D boundary=$boundary \
-#   message.txt.jinja2 > message.txt
-
+    
 # uuencode -m $filepath $(basename $filepath) >> message.txt
 # echo "" >> message.txt
 # echo "--$boundary--" >> message.txt
